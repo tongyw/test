a71c00d4bb402d507e43f28a391171a7e9ead928
tongyw <958003268@qq.com>
2019-07-25 14:28:12 +0800

    do what

---
+++ src/views/login/index.vue
@@ -0,0 +1,167 @@
+<template>
+    <div class="login-container">
+      <el-form class="login-form" autocomplete="on" label-position="left">
+        <div class="title-container">
+          <h3 class="title">Login Form</h3>
+        </div>
+
+        <el-form-item prop="username">
+          <span class="svg-container">
+            <svg-icon icon-class="user" />
+          </span>
+          <el-input placeholder="Username"  name="username" type="text" autocomplete="on"/>
+        </el-form-item>
+
+        <el-tooltip >
+          <el-form-ietm prop="password">
+            <span class="svg-container">
+              <svg-icon icon-class="password" />
+            </span>
+            <el-input  placeholder="Password" name="password"  autocomplete="on"/>
+            <span class="show-pwd" >
+            <svg-icon />
+            </span>
+          </el-form-ietm>
+        </el-tooltip>
+
+        <el-button  type="primary" style="width:100%;margin-bottom:30px;" >Login</el-button>
+
+        <div style="position:relative">
+          <div class="tips">
+            <span>Username : admin</span>
+            <span>Password : any</span>
+          </div>
+          <div class="tips">
+            <span style="margin-right:18px;">Username : editor</span>
+            <span>Password : any</span>
+          </div>
+
+          <el-button class="thirdparty-button" type="primary" >
+            Or connect with
+          </el-button>
+        </div>
+      </el-form>
+    </div>
+</template>
+
+<script>
+export default({
+  data () {
+    return {}
+  },
+  methods: {}
+})
+</script >
+
+<style lang="scss" rel="stylesheet/scss">
+  $bg:#283443;
+  $light_gray:#fff;
+  $cursor: #fff;
+
+  @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
+    .login-container .el-input input {
+      color: $cursor;
+    }
+  }
+
+  .login-container{
+    .el-input{
+      display: inline-block;
+      height: 47px;
+      width: 85%;
+
+      input {
+        background: transparent;
+        border: 0px;
+        -webkit-appearance: none;
+        border-radius: 0px;
+        padding: 12px 5px 12px 15px;
+        color: $light_gray;
+        height: 47px;
+        caret-color: $cursor;
+        &:-webkit-autofill {
+          box-shadow: 0 0 0px 1000px $bg inset !important;
+          -webkit-text-fill-color: $cursor !important;
+        }
+      }
+    }
+
+    .el-form-item {
+      border: 1px solid rgba(255, 255, 255, 0.1);
+      background: rgba(0, 0, 0, 0.1);
+      border-radius: 5px;
+      color: #454545;
+    }
+  }
+</style>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+  $bg:#2d3a4b;
+  $dark_gray:#889aa4;
+  $light_gray:#eee;
+
+  .login-container{
+    min-height: 100%;
+    width: 100%;
+    background-color: $bg;
+    overflow: hidden;
+
+    .login-form {
+      position: relative;
+      width: 520px;
+      max-width: 100%;
+      padding: 160px 35px 0;
+      margin: 0 auto;
+      overflow: hidden;
+    }
+    .tips {
+      font-size: 14px;
+      color: #fff;
+      margin-bottom: 10px;
+      span {
+        &:first-of-type {
+          margin-right: 16px;
+        }
+      }
+    }
+    .svg-container {
+      padding: 6px 5px 6px 15px;
+      color: $dark_gray;
+      vertical-align: middle;
+      width: 30px;
+      display: inline-block;
+    }
+    .title-container {
+      position: relative;
+      .title {
+        font-size: 26px;
+        color: $light_gray;
+        margin: 0px auto 40px auto;
+        text-align: center;
+        font-weight: bold;
+      }
+    }
+
+    .show-pwd {
+      position: absolute;
+      right: 10px;
+      top: 7px;
+      font-size: 16px;
+      color: $dark_gray;
+      cursor: pointer;
+      user-select: none;
+    }
+
+    .thirdparty-button {
+      position: absolute;
+      right: 0;
+      bottom: 6px;
+    }
+
+    @media only screen and (max-width: 470px) {
+      .thirdparty-button {
+        display: none;
+      }
+    }
+  }
+</style>
